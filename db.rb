f143654a2befada10a3ac0101370317f572187a1
ahsaS <a.kvasnikov@a-dobra.ru>
2017-10-29 14:44:54 +0300

    DB WORK

--- db.rb
+++ db.rb
@@ -3,7 +3,13 @@ require 'sqlite3'
 
 configure do
   @db = SQLite3::Database.new 'STORAGE.db'
-  @db.execute 'CREATE TABLE IF NOT EXISTS "STORAGE" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "CATEGORIES" VARCHAR(100), "GIVER" VARCHAR(100), "DATE_OF" VARCHAR(100), "TITLE" VARCHAR(100), "SERIAL" VARCHAR(100), "BALANCE" VARCHAR(100), "ADDINF" VARCHAR(500), LOCATION VARCHAR(100));'
+  @db.execute 'CREATE TABLE IF NOT EXISTS "STORAGE" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "CATEGORIES" VARCHAR(100), "GIVER" VARCHAR(100), "DATE_OF" VARCHAR(100), "TITLE" VARCHAR(100), "SERIAL" VARCHAR(100), "BALANCE" VARCHAR(10), "ADDINF" VARCHAR(500), "LOCATION" VARCHAR(100));'
+end
+
+def get_db
+  db = SQLite3::Database.new 'STORAGE.db'
+  db.results_as_hash = true
+  return db
 end
 
 get '/' do
@@ -15,5 +21,16 @@ get '/tech' do
 end
 
 post '/tech' do
+  @what = params[:what]
+  @whogiver = params[:whogiver]
+  @dtof = params[:dtof]
+  @title = params[:title]
+  @sernum = params[:sernum]
+  @balance = params[:balans]
+  @addinf = params[:addinf]
+  @location = params[:location]
+  db = get_db
+  db.execute 'INSERT INTO STORAGE(CATEGORIES, GIVER, DATE_OF, TITLE, SERIAL, BALANCE, ADDINF, LOCATION) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [@what, @whogiver, @dtof, @title, @sernum, @balance, @addinf, @location]
   erb :technics
+
 end
